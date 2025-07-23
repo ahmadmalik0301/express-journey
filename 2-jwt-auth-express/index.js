61e824e719faee93e8ad6d00e570d7e1bce45085
Muhammad Ahmad <mahmadmalik0301@gmail.com>
2025-07-23 19:25:31 +0500

    Proper port name

--- 2-jwt-auth-express/index.js
+++ 2-jwt-auth-express/index.js
@@ -57,4 +57,6 @@ app.use((req,res,next)=> {
     res.status(404).json({message:'Page not Found'});
 })
 
-app.listen(3001);
+app.listen(3001, () => {
+    console.log('Server running on http://localhost:3001');
+});
