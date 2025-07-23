8c134b2f1b4db339b0c81b26e05f147762144214
Muhammad Ahmad <mahmadmalik0301@gmail.com>
2025-07-23 16:00:27 +0500

    Added Router Folder

--- 1-restful-api-express/index.js
+++ 1-restful-api-express/index.js
@@ -4,6 +4,7 @@ const app = express();
 function sendMessageJson(msg) {
     return { message: msg };
 }
+const productsRouter = require('./Routes/productsRouter');
 
 app.use(express.json());
 
@@ -31,127 +32,11 @@ app.use((req, res, next) => {
     next();
 });
 
-
-let currentId = 5;
-const products = [
-    {
-        id: 1,
-        title: "Wireless Mouse",
-        price: 29.99,
-
-    },
-    {
-        id: 2,
-        title: "Running Shoes",
-        price: 89.99,
-
-    },
-    {
-        id: 3,
-        title: "Smart Watch",
-        price: 129.99,
-
-    },
-    {
-        id: 4,
-        title: "Graphic T-Shirt",
-        price: 15.99,
-    }
-];
-
-
 app.get(`/`, (req, res) => {
     res.status(200).json(sendMessageJson("This is My Home page."));
 });
 
-
-app.get(`/products`, (req, res) => {
-    res.status(200).json({ products });
-});
-
-
-app.get('/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id);
-    const product = products.find(p => p.id === productId);
-
-    if (product) {
-        res.status(200).json({ product });
-    } else {
-        res.status(404).json(sendMessageJson("Product not found"));
-    }
-});
-
-app.post('/products', (req, res) => {
-    console.log(req.body);
-    const { title, price } = req.body;
-
-    const newProduct = {
-        id: currentId++,
-        title: title,
-        price: price
-    };
-
-    products.push(newProduct);
-
-    res.status(201).json({
-        ...sendMessageJson("New Product Added"),
-        product: newProduct
-    });
-});
-
-app.put('/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id);
-    const { title, price } = req.body;
-
-    const productIndex = products.findIndex(p => p.id === productId);
-
-    if (productIndex === -1) {
-        return res.status(404).json(sendMessageJson(`Product with ID ${productId} not found`));
-    }
-
-    products[productIndex] = {
-        id: productId,
-        title,
-        price
-    };
-
-    res.status(200).json({
-        ...sendMessageJson("Product Updated"),
-        product: products[productIndex]
-    });
-});
-
-
-app.delete('/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id);
-
-    const index = products.findIndex(p => p.id === productId);
-    if (index === -1) {
-        return res.status(404).json(sendMessageJson(`Product with ID ${productId} not found`));
-    }
-
-    products.splice(index, 1);
-
-    res.status(200).json(sendMessageJson(`Product with ID ${productId} deleted successfully`));
-});
-
-app.patch('/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id);
-    const { title, price } = req.body;
-
-    const product = products.find(p => p.id === productId);
-    if (!product) {
-        return res.status(404).json(sendMessageJson(`Product with ID ${productId} not found`));
-    }
-
-    if (title !== undefined) product.title = title;
-    if (price !== undefined) product.price = price;
-
-    res.status(200).json({
-        ...sendMessageJson("Product Updated"),
-        product
-    });
-});
+app.use('/products',productsRouter);
 
 // 404 - Not Found (for any unmatched route)
 app.use((req, res, next) => {
