59bac7cbfdb1b7ac0007bab61405f645194369d5
Muhammad Ahmad <mahmadmalik0301@gmail.com>
2025-07-23 19:41:10 +0500

    Some Changes

--- 2-jwt-auth-express/index.js
+++ 2-jwt-auth-express/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
 
+// require('dotenv').config();
+// const SECRET = process.env.SECRET;
+const PORT = 3001;
+
+
 const SECRET = 'secret123';
+
+
 
 app.use(express.json());
 
@@ -57,6 +64,6 @@ app.use((req,res,next)=> {
     res.status(404).json({message:'Page not Found'});
 })
 
-app.listen(3001, () => {
-    console.log('Server running on http://localhost:3001');
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
